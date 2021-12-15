003aeb74f8f0cd885679732f864bc942d1f2ded2
mirsella <mirsella@protonmail.com>
2021-12-15 15:15:58 +0100

    initial commit 🎆

---
+++ index.js
@@ -0,0 +1,40 @@
+const Reddit = require('reddit')
+require('dotenv').config()
+const axios = require('axios');
+const fs = require('fs-extra');
+
+let config = fs.readJsonSync('./config.json');
+const writeconfig = async () => fs.writeJsonSync('./config.json', config)
+const train = fs.readFileSync('train.txt').toString()
+
+const reddit = new Reddit({
+  username: process.env.REDDIT_USERNAME,
+  password: process.env.REDDIT_PASSWORD,
+  appId: process.env.REDDIT_APP_ID,
+  appSecret: process.env.REDDIT_APP_SECRET,
+  userAgent: 'MyApp/1.0.0 (http://example.com)'
+});
+
+(async () => {
+  const last = config.last
+  for (sr of config.sr) {
+    api = `https://api.pushshift.io/reddit/comment/search/?subreddit=${sr}&q=sl&after=${last}` 
+    axios.get(api)
+      .then((res) => {
+        const comments = res.data.data;
+        comments.forEach(comment => {
+          reddit.post('/api/comment', {
+            api_type: 'json',
+            text: train,
+            thing_id: `t1_${comment.id}`
+          })
+            .then(e => {
+              console.log('done', e.json.data.things[0].data.permalink)
+            })
+            .catch(console.error)
+        });
+      })
+  }
+  config.last = new Date().getTime().toString().slice(0, 10);
+  writeconfig();
+})()
