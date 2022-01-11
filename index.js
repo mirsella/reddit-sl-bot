84fa567a9b3d5b00877d43d70aac61bc2e0f2524
mirsella <mirsella@protonmail.com>
2022-01-12 00:12:42 +0100

    added failsafe and improved speed with promise.all

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 const axios = require('axios');
 const fs = require('fs-extra');
 
-let config = fs.readJsonSync('./config.json');
-const writeconfig = async () => fs.writeJsonSync('./config.json', config)
+const config = fs.readJsonSync('./config.json');
+const writeconfig = async () => {
+  fs.writeJsonSync('./config.json', config)
+  console.log('wrote config')
+}
 const train = fs.readFileSync('train.txt').toString()
 
 const reddit = new Reddit({
@@ -15,16 +18,21 @@ const reddit = new Reddit({
   userAgent: 'MyApp/1.0.0 (http://example.com)'
 });
 
+let requests = []
 const post = config.post || false;
 (async () => {
   const last = config.last
-  for (sr of config.sr) {
-    api = `https://api.pushshift.io/reddit/comment/search?subreddit=${sr}&q=sl&after=${last}` 
-    await axios.get(api)
+  config.sr.forEach(async (sr) => {
+    api = `https://api.pushshift.io/reddit/comment/search?subreddit=${sr}&q=sl&after=${last}`;
+    requests.push(axios.get(api)
       .then((res) => {
         const comments = res.data.data;
         if (post == true) {
           for (comment of comments) {
+            if (config.done.includes(comment.id)) {
+              console.log('already done', comment.id)
+              continue;
+            }
             reddit.post('/api/comment', {
               api_type: 'json',
               text: train,
@@ -33,12 +41,17 @@ const post = config.post || false;
               .then(e => {
                 console.log('done', 'https://reddit.com' + e.json.data.things[0].data.permalink)
                 config.last = new Date().getTime().toString().slice(0, 10);
+                config.done.push(comment.id);
               })
               .catch(console.error)
           }
         } else {
+          console.log(sr, comments)
         }
       })
-  }
-  writeconfig();
+    )
+  })
+  Promise.all(requests).then(() => {
+    writeconfig()
+  })
 })()
